1544efbc7cbcafc5372bf8d1390611044ba11c43
Bhushan kakade <kakadebhushan467@gmail.com>
2024-05-20 15:59:54 +0530

    project setup Adding the Dependencies required for project

---
+++ smartcontactmanegerproject/src/main/java/com/smart/SmartcontactmanegerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcontactmanegerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcontactmanegerprojectApplication.class, args);
+	}
+
+}
